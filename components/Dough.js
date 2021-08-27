ab956dddf6b4f86a2cd96db34fdc66746856af99
Andrew Sharkey <sharkand@hotmail.com>
2021-08-27 08:02:10 +0100

    Styling added to Dough page.

--- components/Dough.js
+++ components/Dough.js
@@ -5,20 +5,64 @@ import NumericInput from 'react-native-numeric-input';
 const Dough = () => {
     const [doughWeight, setDoughWeight] = useState(1000);
     return (
-        <View>
-            <View>
+        <View style={styles.container}>
+            <View style={styles.header}></View>
+            <View style={styles.body}>
                 <Text>
                 Weight of dough?
                 </Text>
                 <NumericInput value = {doughWeight} 
                 onChange = {value => setDoughWeight(value)}
                 />
-            </View>
-            <TouchableOpacity>
+                <TouchableOpacity>
                 <Text>Next &gt;&gt;</Text>
             </TouchableOpacity>
+            </View>
+            <View style={styles.footer}></View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white'
+    },
+    header: {
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 10,
+        borderBottomColor: '#00ee00',
+        paddingTop: 20,
+        height: '20%'
+    },
+    headerText: {
+        color: '#FFD700',
+        fontSize: 36,
+        padding: 26,
+        fontWeight: "500",
+    },
+    body: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '62%'
+    },
+    button: {
+        padding: 15,
+        backgroundColor: 'grey',
+        borderRadius: 20
+    },
+    buttonText: {
+        fontSize: 40,
+        fontWeight: '500'
+    },
+    footer: {
+        backgroundColor: 'red',
+        borderTopWidth: 10,
+        borderTopColor: '#ff6666',
+        height: '18%'    
+    }
+  });
 
 export default Dough;
